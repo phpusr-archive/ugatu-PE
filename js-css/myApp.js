3e7a10b0a6427503cc7b16326494bba7cb1a8ed2
phpusr <phpusr@gmail.com>
2013-12-27 12:42:21 +0600

    add: Генерация рандомных значений

--- js-css/myApp.js
+++ js-css/myApp.js
@@ -13,23 +13,24 @@ app.controller('MyCtrl', function MyCtrl($scope) {
             //Заголовки
             rows = $scope.rows+1;
             columns = $scope.columns+1;
+            var maxValue = 20;
 
             //Заполнение заголовков
             $scope.table = [];
             $scope.table[0] = [];
             $scope.table[0][0] = new Data('X', true);
             for (var j=1; j<columns; j++) {
-                $scope.table[0][j] = new Data('Param '+j, true);
+                $scope.table[0][j] = new Data('', true);
             }
             for (var i=1; i<rows; i++) {
                 $scope.table[i] = [];
-                $scope.table[i][0] = new Data(i, true);
+                $scope.table[i][0] = new Data(i==rows-1 ? 'Y' : i, true);
             }
 
             //Заполнение нулями
             for (i=1; i<rows; i++) {
                 for (j=1; j<columns; j++) {
-                    $scope.table[i][j] = new Data(0, false);
+                    $scope.table[i][j] = new Data(Math.random()*maxValue, false);
                 }
             }
 
@@ -51,9 +52,9 @@ app.controller('MyCtrl', function MyCtrl($scope) {
         $scope.changeParams();
         loadData([
 			'X', '', '', '', '', '',
-            '', 2.132,2.114,2.160,2.146,2.120,
-            '', 3.373,3.324,3.377,3.327,3.358,
-            '', 3.978,3.928,3.905,3.948,3.904,
+            '1', 2.132,2.114,2.160,2.146,2.120,
+            '2', 3.373,3.324,3.377,3.327,3.358,
+            '3', 3.978,3.928,3.905,3.948,3.904,
             'Y', 6.898,6.908,6.887,6.940,6.904
         ]);
         $scope.changeTable();
